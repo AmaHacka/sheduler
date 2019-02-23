4fe9c3a8ae5444ec0db3e5c20fd272bd9e1cfab0
egormalysev <egorpw@yandex.ru>
2019-02-23 19:48:42 +0300

    Worker and Day models

--- sheduler/timetable/models.py
+++ sheduler/timetable/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+WEEKDAYS = [
+    (1, ("Monday")),
+    (2, ("Tuesday")),
+    (3, ("Wednesday")),
+    (4, ("Thursday")),
+    (5, ("Friday")),
+    (6, ("Saturday")),
+    (7, ("Sunday")),
+]
+
+
+class Worker(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    full_day = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Day(models.Model):
+    weekday = models.IntegerField(
+        choices=WEEKDAYS,
+        unique=True
+    )
+
+    _8_9 = models.BooleanField(default=False)
+    _9_10 = models.BooleanField(default=False)
+    _10_11 = models.BooleanField(default=False)
+    _11_12 = models.BooleanField(default=False)
+    _12_13 = models.BooleanField(default=False)
+    _13_14 = models.BooleanField(default=False)
+    _14_15 = models.BooleanField(default=False)
+    _15_16 = models.BooleanField(default=False)
+    _16_17 = models.BooleanField(default=False)
+    _17_18 = models.BooleanField(default=False)
+    _19_20 = models.BooleanField(default=False)
+    _20_21 = models.BooleanField(default=False)
+
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
--- sheduler/timetable/views.py
+++ sheduler/timetable/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from django.template import loader
+
+from .models import Worker
+
+
+def index(request):
+    workers = Worker.objects.all()
+    template = loader.get_template('timetable/index.html')
+    context = {
+        'workers_list': workers,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def worker(request, user_id):
+    return HttpResponse("Here detailed user view")
