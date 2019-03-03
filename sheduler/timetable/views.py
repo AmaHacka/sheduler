363194c410b1ea9e8070d5156756b54d2cc04096
egormalysev <egorpw@yandex.ru>
2019-03-04 00:09:42 +0300

    Odd and eve weeks

--- sheduler/timetable/views.py
+++ sheduler/timetable/views.py
@@ -4,6 +4,34 @@ from .models import Worker, Day
 from django.db.models import Q
 
 
+class TemplateDay:
+    def __init__(self, name):
+        self.name = name
+        self.odd = None
+        self.even = None
+        self.split = False
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        if self.split:
+            if self.odd and self.even:
+                return "+"
+            if self.odd:
+                return "+/-"
+            if self.even:
+                return "-/+"
+
+        else:
+            if self.odd or self.even:
+                return "+"
+        return " "
+
+    def __repr__(self):
+        return f"<TemplateDay object {self.name} odd: {self.odd} even: {self.even}> split: {self.split}"
+
+
 class IndexView(generic.TemplateView):
     template_name = "timetable/index.html"
 
@@ -24,6 +52,28 @@ class WorkerView(generic.TemplateView):
         hours = [a for a in Day.__dict__.keys() if a.startswith("h")]
         # В стоках - часы работы, в столбцах - дни
         # Day._meta.get_field(h).verbose_name - взять атрибут verbose_name
-        table = {Day._meta.get_field(h).verbose_name: [getattr(d, h) for d in days] for h in hours}
+        table = {Day._meta.get_field(h).verbose_name: self.construct_hours(days, h) for h in hours}
         context["table"] = table
         return context
+
+    def construct_hours(self, days, hour_attr):
+        days_templte = []
+        for day in days:
+            t_day = TemplateDay(day.weekday)
+            if t_day not in days_templte:
+                if day.odd:
+                    t_day.odd = getattr(day, hour_attr)
+                else:
+                    t_day.even = getattr(day, hour_attr)
+                days_templte.append(t_day)
+            else:
+                for d in days_templte:
+                    if d == t_day:
+                        d.split = True
+                        if day.odd:
+                            d.odd = getattr(day, hour_attr)
+                        else:
+                            d.even = getattr(day, hour_attr)
+        return map(lambda x: str(x), days_templte)
+
+
